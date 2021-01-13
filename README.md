5389e8204bda813add417c6e0b027c7be9cdc8e2
Leonardo Almeida <leonardo.teles.almeida@gmail.com>
2021-01-13 07:04:03 -0300

    Atualizando Títulos

---
+++ README.md
@@ -0,0 +1,12 @@
+# ➤ Angular 9 - Essencial
+
+CRUD completo de produtos desenvolvido como exemplo no treinamento de Angular 9 da Cod3r.
+
+### ➤ Status do Projeto
+
+<img src="https://img.shields.io/badge/CRUD%20de%20Produtos-100%25-success"/>
+
+### ➤ Imagens do Sistema
+
+<img src="./frontend/src/assets/img/screenshots/home.jpg">
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -27,7 +27,7 @@ import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule }  from '@angular/material/form-field';
 import { MatInputModule }  from '@angular/material/input';
 import { ProductReadComponent } from './components/product/product-read/product-read.component';
-import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -50,7 +50,6 @@ registerLocaleData(localePt);
     RedDirective,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component,
     ProductUpdateComponent,
     ProductDeleteComponent
   ],
